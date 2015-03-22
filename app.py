594eb2f1b5be115293708c477048c7d004464f5e
randy <randyh1981@gmail.com>
2015-03-21 23:51:45 -0400

    devkey

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app.config['SECRET_KEY'] = 'devkey'
 @app.route('/')
 def index():
     return 'Hello World!'
